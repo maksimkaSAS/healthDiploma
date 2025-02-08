723c4c7ebca4970608f44c7abdad2f38d5bfd5a3
maksimkaSAS <knms1b22.sas@kpnu.edu.ua>
2025-02-08 12:34:00 +0200

    add drug selector to pharmacy page

--- src/app/pages/user/pharmacy/pharmacy.component.ts
+++ src/app/pages/user/pharmacy/pharmacy.component.ts
@@ -11,16 +11,25 @@ import { HealthpharmacyService } from 'src/app/modules/healthpharmacy/services/h
 	standalone: false
 })
 export class PharmacyComponent {
+	//TODO drug_id = '';
+	drug_id = '';
 	pharmacies: Healthpharmacy[] = [];
 
 	constructor(
 		private _healthpharmacyService: HealthpharmacyService,
 		private _form: FormService
-	) {this.load();}
+	) {
+		this.load();
+	}
 
 	load(): void {
 		this._healthpharmacyService
-			.get({}, { name: 'public' })
+			.get(
+				{
+					query: this.drug_id ? 'drug=' + this.drug_id : ''
+				},
+				{ name: 'public' }
+			)
 			.subscribe((pharmacies) => {
 				this.pharmacies.splice(0, this.pharmacies.length);
 				this.pharmacies.push(...pharmacies);
@@ -28,29 +37,29 @@ export class PharmacyComponent {
 	}
 
 	form: FormInterface = this._form.getForm(
-			'clinic',
-			healthpharmacyFormComponents
-		);
+		'clinic',
+		healthpharmacyFormComponents
+	);
 
-		create(): void {
-				this._form.modal<Healthpharmacy>(this.form, {
-					label: 'Create',
-					click: async (
-						created: unknown,
-						close: () => void
-					): Promise<void> => {
-						close();
-		
-						this._healthpharmacyService
-							.create(created as Healthpharmacy)
-							.subscribe(() => {
-								this.load();
-							});
-		
-						close();
-					}
-				});
+	create(): void {
+		this._form.modal<Healthpharmacy>(this.form, {
+			label: 'Create',
+			click: async (
+				created: unknown,
+				close: () => void
+			): Promise<void> => {
+				close();
+
+				this._healthpharmacyService
+					.create(created as Healthpharmacy)
+					.subscribe(() => {
+						this.load();
+					});
+
+				close();
 			}
+		});
+	}
 
 	/*ngOnInit(): void {
 		this._healthpharmacyService
--- src/app/pages/user/pharmacy/pharmacy.module.ts
+++ src/app/pages/user/pharmacy/pharmacy.module.ts
@@ -3,16 +3,22 @@ import { CoreModule } from 'src/app/core/core.module';
 import { PharmacyComponent } from './pharmacy.component';
 import { Routes, RouterModule } from '@angular/router';
 import { MypharmacyComponent } from './mypharmacy/mypharmacy.component';
+import { SelectUserComponent } from 'src/app/modules/healthdrug/selectors/healthdrug/healthdrug-selector.component';
 
 const routes: Routes = [
 	{
 		path: '',
 		component: PharmacyComponent
+	},
+
+	{
+		path: ':drug_id',
+		component: PharmacyComponent
 	}
 ];
 
 @NgModule({
-	imports: [RouterModule.forChild(routes), CoreModule],
+	imports: [RouterModule.forChild(routes), CoreModule, SelectUserComponent],
 	declarations: [PharmacyComponent, MypharmacyComponent]
 })
 export class PharmacyModule {}
