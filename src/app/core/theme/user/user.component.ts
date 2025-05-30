295e5963a33ae8f152015c21b840575d5e203010
maksimkaSAS <knms1b22.sas@kpnu.edu.ua>
2025-05-30 17:01:38 +0300

    added language switcher

--- src/app/core/theme/user/user.component.ts
+++ src/app/core/theme/user/user.component.ts
@@ -3,13 +3,17 @@ import { coreAnimation } from '../../animations/core.animations';
 import { environment } from 'src/environments/environment';
 import { Platform } from '@angular/cdk/platform';
 import { Component } from '@angular/core';
+import {
+	TranslateService,
+	Language
+} from 'src/app/core/modules/translate/translate.service';
 
 @Component({
-    selector: 'app-user',
-    templateUrl: './user.component.html',
-    styleUrls: ['./user.component.scss'],
-    animations: [coreAnimation],
-    standalone: false
+	selector: 'app-user',
+	templateUrl: './user.component.html',
+	styleUrls: ['./user.component.scss'],
+	animations: [coreAnimation],
+	standalone: false
 })
 export class UserComponent {
 	readonly url = environment.url;
@@ -21,5 +25,19 @@ export class UserComponent {
 		}
 	}
 
-	constructor(public us: UserService, private _platform: Platform) {}
+	constructor(
+		public us: UserService,
+		private _platform: Platform,
+		public ts: TranslateService
+	) {}
+
+	getTranslatedText(toTranslate: string) {
+		return this.ts.translate(toTranslate);
+	}
+
+	set_language(code: string) {
+		this.ts.set_language(
+			this.ts.languages.find((l) => l.code === code) as Language
+		);
+	}
 }
